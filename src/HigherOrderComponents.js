f1caa32189e41574913c334c2769bfb04ff8a047
Matt Vegh <mdv240@gmail.com>
2020-02-18 15:26:29 -0600

    practice

---
+++ src/HigherOrderComponents.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+//Stems from the concept of higher order functions
+
+//A function that takes a component as its first arugment and returns a new component that wraps the given component, 
+//providing extra capabilities to it
+
