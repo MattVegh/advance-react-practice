a004087820b0cf283ad241d504ef8acd8ff497bf
Matt Vegh <mdv240@gmail.com>
2020-02-18 21:45:04 -0600

    HOCPART4

--- src/HOC_Part_4/withToggler.js
+++ src/HOC_Part_4/withToggler.js
@@ -1,6 +1,31 @@
-import React from 'react'
+import React, { Component } from 'react'
 
-export function withTogger(component) {
+
+class Toggler extends Component {
+    state = {
+        on: false
+    }
+
+    toggle = () => {
+        this.setState(prevState => {
+            return {
+                on: !prevState.on
+            }
+        })
+    }
+
+    render() {
+        const C = this.props.component
+        return (
+            <C 
+            on={this.state.on} 
+            toggle={this.toggle}
+            {...this.props} 
+            />
+        )
+    }
+}
+export function withToggler(component) {
     
     return function(props) {
         return (
