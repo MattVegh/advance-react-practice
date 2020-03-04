e4fd29cfb82d999c12231ef543d5cf0d5d138788
Matt Vegh <mdv240@gmail.com>
2020-03-03 20:41:00 -0600

    typo

--- src/App.js
+++ src/App.js
@@ -37,13 +37,13 @@ function App(props) {
       
       <hr />
       <DataFetcher url="https://swapi.co/api/people/1">
-          {({data, loading}) => {
-            return (
+          {({data, loading}) => 
+            (
               loading ?
               <h1>Loading...</h1> :
               <p>{JSON.stringify(data)}</p>
             )
-          }}
+          }
       </DataFetcher>
       <Favorite />
       <Menu />
