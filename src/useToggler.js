6357b8c96382f341fc9a882c9e30b28d74096695
Matt Vegh <mdv240@gmail.com>
2020-03-28 13:52:57 -0500

    custom useToggle

--- src/useToggler.js
+++ src/useToggler.js
@@ -1,7 +1,13 @@
 import {useState} from "react"
 
 function useToggler() {
-    const [ toggle, setToggle] = useState('false')
+    const [ isToggledOn, setIsToggledOn ] = useState(false)
+
+    function toggle() {
+        setIsToggledOn(!isToggledOn)
+    }
+
+    return { isToggledOn, toggle }
 }
 
 export default useToggler
